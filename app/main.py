5398b789c984a2d31a7b368ff29fa90d6e832fef
Meli Gloria <meligloria0@gmail.com>
2025-10-04 02:11:37 +0100

    fix: black formatting issues

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-
 @app.get("/")
 def root():
     return {"msg": "Restaurant API is running 🚀"}
